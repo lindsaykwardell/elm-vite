62b79e1c13f1a8adc94f87f399135ae8589825c9
Lindsay Wardell <three060@gmail.com>
2022-01-04 11:23:47 -0800

    Add degit as a dep

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ import { execa } from "execa";
 const loc = process.argv[2] || ".";
 
 async function main() {
-  const { stderr } = await execa("npx", [
-    "degit",
+  const { stderr } = await execa("degit", [
     "lindsaykwardell/vite-elm-template",
     loc,
   ]);
